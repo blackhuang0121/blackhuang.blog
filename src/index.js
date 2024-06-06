0c68a9f12e289fc43d396d937139752242939632
“blackhuang0121git config --global user.email “huhu76543212001@gmail.com <e0121n@huangqihaodeMacBook-Air.local>
2024-06-06 19:35:00 +0800

    feat: homepage-v3

--- src/index.js
+++ src/index.js
@@ -2,3 +2,28 @@ import { someUtils } from './another';
 
 document.body.innerHTML += '<p class="text-red-500">This is a paragraph added by JavaScript from src/index.js</p>';
 document.body.innerHTML += someUtils();
+
+// // addEventListener('click', ())
+
+// const buttonElement = document.getElementById('btn');
+
+// // 透過提供回呼函數的方式對「click」事件新增處理器。
+// // 當元素被點選後會出現「Element clicked!」的彈出訊息。
+
+// // eslint-disable-next-line no-console
+// buttonElement.addEventListener('click', () => {
+//   console.log('Element clicked through function!');
+// });
+
+// // 基於相容性，一個帶有 `handleEvent` 的非函式物件可被視為處理函式。
+
+// // eslint-disable-next-line no-console
+// buttonElement.addEventListener('click', () => {
+//   const handler = {
+//     handleEvent() {
+//       console.log('Element clicked through handleEvent property!');
+//     },
+//   };
+
+//   handler.handleEvent(); // 调用 handleEvent 方法
+// });
